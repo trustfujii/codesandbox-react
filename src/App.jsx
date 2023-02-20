692fc85a97badb3414a63c1a09546bad97cd12c8
trustfujii <125522394+trustfujii@users.noreply.github.com>
2023-02-20 10:40:14 +0900

    セクション6完了

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,44 @@
-import React, { useState } from "react";
-import ColorfulMessage from "./components/ColorfulMessage";
+/* eslint react-hooks/exhaustive-deps: off */
+import React, { useEffect, useState } from "react";
+import { ColorfulMessage } from "./components/ColorfulMessage";
 
 const App = () => {
+  console.log("最初");
+  // State記述 [変数名, 関数名]自由に命名できる　初期値0
+  const [num, setNum] = useState(0);
+  const [faceShowFlag, setFaceShowFlag] = useState(false);
+
   const onClickCountUp = () => {
     // 1カウント
-    setNum(num + 1);
+    return setNum(num + 1);
   };
-  // [変数名, 関数名]自由に命名できる　初期値0
-  const [num, setNum] = useState(0);
+
+  const onClickSwitchShowFlag = () => {
+    // on offを切替
+    return setFaceShowFlag(!faceShowFlag);
+  };
+
+  useEffect(() => {
+    if (num > 0) {
+      if (num % 3 === 0) {
+        faceShowFlag || setFaceShowFlag(true);
+      } else {
+        faceShowFlag && setFaceShowFlag(false);
+      }
+    }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [num]);
+
   return (
     <React.Fragment>
       <h1 style={{ color: "red" }}>こんにちは</h1>
       <ColorfulMessage color="blue">お元気ですか？</ColorfulMessage>
       <ColorfulMessage color="pink">元気です</ColorfulMessage>
       <button onClick={onClickCountUp}>カウントアップ</button>
+      <br />
+      <button onClick={onClickSwitchShowFlag}>on/off</button>
       <p>{num}</p>
+      {faceShowFlag && <p>(´・ω・)</p>}
     </React.Fragment>
   );
 };
--- src/components/ColorfulMessage.jsx
+++ src/components/ColorfulMessage.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 
-const ColorfulMessage = (props) => {
+export const ColorfulMessage = (props) => {
   const { color, children } = props;
   const contentStyle = {
     color: color,
@@ -9,4 +9,4 @@ const ColorfulMessage = (props) => {
   return <p style={contentStyle}>{children}</p>;
 };
 
-export default ColorfulMessage;
+// export default ColorfulMessage;
